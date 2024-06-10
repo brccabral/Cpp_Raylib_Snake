81fcc5a6c35526bddc1e9d825da0f409cd291b31
Bruno Cabral <cabral.brc@gmail.com>
2024-06-09 20:35:48 -0700

    compile web with resources

--- build_web.sh
+++ build_web.sh
@@ -2,6 +2,6 @@
 
 source $EMSDK/emsdk_env.sh
 
-emcc -o snake.html main.cpp -Os -Wall $RAYLIB_INSTALL_PATH/lib/libraylib.a -I. -I$RAYLIB_INSTALL_PATH/include -L. -L$RAYLIB_INSTALL_PATH/lib -s USE_GLFW=3 -s ASYNCIFY --shell-file $RAYLIB_SRC_PATH/shell.html -DPLATFORM_WEB
+emcc -o snake.html main.cpp -Os -Wall $RAYLIB_INSTALL_PATH/lib/libraylib.a -I. -I$RAYLIB_INSTALL_PATH/include -L. -L$RAYLIB_INSTALL_PATH/lib -s USE_GLFW=3 -s ASYNCIFY --shell-file $RAYLIB_SRC_PATH/shell.html -DPLATFORM_WEB --preload-file resources/Graphics --preload-file resources/Sounds
 
 # python -m http.server 8080
